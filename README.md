fc56d85e494edae00f1853c12ea6c11bc6a503b8
Esad Cetiner <104706115+EsadCetiner@users.noreply.github.com>
2025-11-18 19:32:01 +1100

    refactor: move config files out of main script

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ This script sets up a dev environment ready with all the tools needed for develo
 This has only been tested on Ubuntu 24.04 so this script might not work for other distros.
 
 No rules are shipped with this script, and http services are disabled. You are expected to bring your own and start/stop the services as needed.
+
+This script can be ran with this one-liner:
+```
+curl -s https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/setup.sh | bash
+```
--- setup.sh
+++ setup.sh
@@ -12,23 +12,7 @@ modsecurity()
   echo "deb http://modsecurity.digitalwave.hu/ubuntu/ $(lsb_release -sc)-backports main" | sudo tee /etc/apt/sources.list.d/dwmodsec.list
 
   # Prefer digitalwave repository for modsec
-  cat << EOF | sudo tee /etc/apt/preferences.d/99modsecurity
-Package: *libnginx-mod-http-modsecurity*
-Pin: origin modsecurity.digitalwave.hu
-Pin-Priority: 900
-
-Package: *libapache2-mod-security2*
-Pin: origin modsecurity.digitalwave.hu
-Pin-Priority: 900
-
-Package: *modsecurity-crs*
-Pin: origin modsecurity.digitalwave.hu
-Pin-Priority: 900
-
-Package: *libmodsecurity*
-Pin: origin modsecurity.digitalwave.hu
-Pin-Priority: 900
-EOF
+  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/apt/99modsecurity -O /etc/apt/preferences.d/99modsecurity
 
   sudo apt update
 
@@ -40,42 +24,10 @@ EOF
 
   sudo cp /etc/modsecurity/modsecurity.conf-recommended /etc/modsecurity/modsecurity.conf
 
-  cat <<EOF | sudo tee /etc/modsecurity/main.conf
-include /etc/modsecurity/coreruleset/crs-setup.conf
-include /etc/modsecurity/dev.conf
-include /etc/modsecurity/coreruleset/plugins/*-config.conf
-include /etc/modsecurity/coreruleset/plugins/*-before.conf
-include /etc/modsecurity/modsecurity.conf
-include /etc/modsecurity/coreruleset/rules/*.conf
-include /etc/modsecurity/coreruleset/plugins/*-after.conf
-EOF
+  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/modsecurity/main.conf -O /etc/modsecurity/main.conf
 
   # See: https://coreruleset.org/docs/6-development/6-5-testing-the-rule-set/
-  cat <<EOF | sudo tee /etc/modsecurity/dev.conf
-SecAction \\
-    "id:900005,\\
-    phase:1,\\
-    nolog,\\
-    pass,\\
-    ctl:ruleEngine=DetectionOnly,\\
-    ctl:ruleRemoveById=910000,\\
-    setvar:tx.blocking_paranoia_level=4,\\
-    setvar:tx.crs_validate_utf8_encoding=1,\\
-    setvar:tx.arg_name_length=100,\\
-    setvar:tx.arg_length=400,\\
-    setvar:tx.total_arg_length=64000,\\
-    setvar:tx.max_num_args=255,\\
-    setvar:tx.max_file_size=64100,\\
-    setvar:tx.combined_file_sizes=65535"
-
-SecRule REQUEST_HEADERS:X-CRS-Test "@rx ^.*$" \\
-    "id:999999,\\
-    phase:1,\\
-    pass,\\
-    t:none,\\
-    log,\\
-    msg:'%{MATCHED_VAR}'"
-EOF
+  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/modsecurity/dev.conf -O tee /etc/modsecurity/dev.conf
 
   # Configure ModSecurity
   sudo sed -i -E "s/SecRuleEngine.*/SecRuleEngine On/" /etc/modsecurity/modsecurity.conf
@@ -91,76 +43,19 @@ EOF
 nginx()
 {
 
-  cat <<EOF | sudo tee /etc/nginx/sites-enabled/default
-server
-{
-    listen 8080 default_server;
-    listen [::]:8080 default_server;
-    server_name _;
-
-    root /var/www/html/;
-    index index.html;
-
-    location /reflect
-    {
-        proxy_pass http://127.0.0.1:8000;
-        proxy_set_header Host \$host;
-        proxy_set_header Proxy "";
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Port \$server_port;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-
-        proxy_http_version 1.1;
-        proxy_buffering off;
-        proxy_connect_timeout 60s;
-        proxy_read_timeout 36000s;
-        proxy_redirect off;
-
-        proxy_pass_header Authorization;
-    }
-
-    error_log /var/log/nginx/error.log info;
-}
-EOF
-
+  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/nginx/default -O /etc/nginx/sites-enabled/default
+  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/nginx/modsecurity.conf -O /etc/nginx/conf.d/modsecurity.conf
   sudo systemctl disable nginx --now
-
-# Configure NGINX to use ModSecurity
-  cat << EOF | sudo tee /etc/nginx/conf.d/modsecurity.conf
-modsecurity on;
-modsecurity_rules_file /etc/modsecurity/main.conf;
-EOF
 
 }
 
 httpd()
 {
 
-  # Make apache2 use main modsec file
-  cat <<EOF | sudo tee /etc/apache2/mods-enabled/security2.conf
-<IfModule security2_module>
-  # Default Debian dir for modsecurity's persistent data
-  SecDataDir /var/cache/modsecurity
-
-  Include /etc/modsecurity/main.conf
-</IfModule>
-EOF
-
   sudo a2enmod proxy proxy_http
-  cat <<EOF | sudo tee /etc/apache2/sites-enabled/000-default.conf
-<VirtualHost *:80>
-  DocumentRoot /var/www/html/
 
-  ProxyPreserveHost On
-  ProxyPass /reflect http://127.0.0.1:8000/reflect
-  ProxyPassReverse /reflect http://127.0.0.1:8000/reflect
-  ServerName localhost
-
-  ErrorLog \${APACHE_LOG_DIR}/error.log
-  CustomLog \${APACHE_LOG_DIR}/access.log combined
-</VirtualHost>
-EOF
+  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/httpd/security.conf -O /etc/apache2/mods-enabled/security2.conf
+  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/httpd/security.conf -O /etc/apache2/sites-enabled/000-default.conf
 
   sudo systemctl disable apache2 --now
 
@@ -174,6 +69,7 @@ crs_tools()
   crs_toolchain_download="$(curl -s https://api.github.com/repos/coreruleset/crs-toolchain/releases/latest | jq -r ".assets[].browser_download_url" | grep "amd64.deb")"
   albedo_download="$(curl -s https://api.github.com/repos/coreruleset/albedo/releases/latest | jq -r ".assets[].browser_download_url" | grep "amd64.deb")"
 
+  # Install tools
   tmp_dir="$(mktemp -d)"
   wget "$go_ftw_download" -O "$tmp_dir/ftw-latest.deb"
   wget "$crs_toolchain_download" -O "$tmp_dir/crs-toolchain-latest.deb"
@@ -182,41 +78,13 @@ crs_tools()
   sudo dpkg -i "$tmp_dir/crs-toolchain-latest.deb"
   sudo dpkg -i "$tmp_dir/albedo-latest.deb"
  
-  cat <<EOF | sudo tee /etc/systemd/system/albedo.service
-[Unit]
-Description=Albedo is a simple HTTP server used as a reverse-proxy backend in testing web application firewalls (WAFs).
-Documentation=https://github.com/coreruleset/albedo
-ConditionFileIsExecutable=/usr/bin/albedo
+  # ftw config
+  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/ftw/.ftw.apache.yaml -O /etc/modsecurity/.ftw.apache.yaml
+  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/ftw/.ftw.nginx.yaml -O /etc/modsecurity/.ftw.nginx.yaml
 
-[Service]
-ExecStart=/usr/bin/albedo -p 8000 -b localhost
-DynamicUser=true
-
-[Install]
-WantedBy=apache2.service nginx.service
-EOF
-
+  # Albedo systemd config
+  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/albedo/albedo.service -O /etc/systemd/system/albedo.service
   sudo systemctl daemon-reload
-
-  # FTW configs for apache and NGINX
-  cat <<EOF | sudo tee /etc/modsecurity/.ftw.apache.yaml
-logfile: /var/log/apache2/error.log
-logmarkerheadername: X-CRS-TEST
-testoverride:
-  input:
-    dest_addr: "127.0.0.1"
-    port: 80
-EOF
-
-  cat <<EOF | sudo tee /etc/modsecurity/.ftw.nginx.yaml
-logfile: /var/log/nginx/error.log
-logmarkerheadername: X-CRS-TEST
-testoverride:
-  input:
-    dest_addr: "127.0.0.1"
-    port: 8080
-EOF
-
   sudo systemctl disable albedo --now
 
 }
