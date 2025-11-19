734cc62477ef5dadfcb96c45247e902d5ecad4a7
Esad Cetiner <104706115+EsadCetiner@users.noreply.github.com>
2025-11-19 12:32:50 +1100

    chore: reduce stdout spam

--- setup.sh
+++ setup.sh
@@ -6,28 +6,28 @@ trap 'echo "Error on line $LINENO with command: $BASH_COMMAND"; exit 1' ERR
 modsecurity()
 {
   # See: https://modsecurity.digitalwave.hu/
-  sudo apt update
-  sudo apt-get -y install apt-transport-https lsb-release ca-certificates curl
+  sudo apt update -q
+  sudo apt-get -y -q install apt-transport-https lsb-release ca-certificates curl
   sudo wget -q https://modsecurity.digitalwave.hu/dwmodsec.gpg -O /etc/apt/trusted.gpg.d/modsecurity-digitalwave.gpg
   echo "deb http://modsecurity.digitalwave.hu/ubuntu/ $(lsb_release -sc)-backports main" | sudo tee /etc/apt/sources.list.d/dwmodsec.list
 
   # Prefer digitalwave repository for modsec
-  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/apt/99modsecurity -O /etc/apt/preferences.d/99modsecurity
+  sudo wget -q https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/apt/99modsecurity -O /etc/apt/preferences.d/99modsecurity
 
   sudo apt update
 
   # Make sure both Apache and NGINX is disabled to avoid errors
-  sudo apt install -y --no-install-recommends apache2 libapache2-mod-security2
+  sudo apt install -y -q --no-install-recommends apache2 libapache2-mod-security2
   sudo systemctl disable apache2 --now
-  sudo apt install -y --no-install-recommends nginx libnginx-mod-http-modsecurity
+  sudo apt install -y -q --no-install-recommends nginx libnginx-mod-http-modsecurity
   sudo systemctl disable nginx --now
 
   sudo cp /etc/modsecurity/modsecurity.conf-recommended /etc/modsecurity/modsecurity.conf
 
-  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/modsecurity/main.conf -O /etc/modsecurity/main.conf
+  sudo wget -q https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/modsecurity/main.conf -O /etc/modsecurity/main.conf
 
   # See: https://coreruleset.org/docs/6-development/6-5-testing-the-rule-set/
-  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/modsecurity/dev.conf -O /etc/modsecurity/dev.conf
+  sudo wget -q https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/modsecurity/dev.conf -O /etc/modsecurity/dev.conf
 
   # Configure ModSecurity
   sudo sed -i -E "s/SecRuleEngine.*/SecRuleEngine On/" /etc/modsecurity/modsecurity.conf
@@ -43,8 +43,8 @@ modsecurity()
 nginx()
 {
 
-  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/nginx/default -O /etc/nginx/sites-enabled/default
-  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/nginx/modsecurity.conf -O /etc/nginx/conf.d/modsecurity.conf
+  sudo wget -q https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/nginx/default -O /etc/nginx/sites-enabled/default
+  sudo wget -q https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/nginx/modsecurity.conf -O /etc/nginx/conf.d/modsecurity.conf
   sudo systemctl disable nginx --now
 
 }
@@ -54,8 +54,8 @@ httpd()
 
   sudo a2enmod proxy proxy_http
 
-  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/httpd/security2.conf -O /etc/apache2/mods-enabled/security2.conf
-  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/httpd/000-default.conf -O /etc/apache2/sites-enabled/000-default.conf
+  sudo wget -q https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/httpd/security2.conf -O /etc/apache2/mods-enabled/security2.conf
+  sudo wget -q https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/httpd/000-default.conf -O /etc/apache2/sites-enabled/000-default.conf
   sudo systemctl disable apache2 --now
 
 }
@@ -70,19 +70,19 @@ crs_tools()
 
   # Install tools
   tmp_dir="$(mktemp -d)"
-  wget "$go_ftw_download" -O "$tmp_dir/ftw-latest.deb"
-  wget "$crs_toolchain_download" -O "$tmp_dir/crs-toolchain-latest.deb"
-  wget "$albedo_download" -O "$tmp_dir/albedo-latest.deb"
+  wget -q "$go_ftw_download" -O "$tmp_dir/ftw-latest.deb"
+  wget -q "$crs_toolchain_download" -O "$tmp_dir/crs-toolchain-latest.deb"
+  wget -q "$albedo_download" -O "$tmp_dir/albedo-latest.deb"
   sudo dpkg -i "$tmp_dir/ftw-latest.deb"
   sudo dpkg -i "$tmp_dir/crs-toolchain-latest.deb"
   sudo dpkg -i "$tmp_dir/albedo-latest.deb"
- 
+
   # ftw config
-  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/ftw/.ftw.apache.yaml -O /etc/modsecurity/.ftw.apache.yaml
-  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/ftw/.ftw.nginx.yaml -O /etc/modsecurity/.ftw.nginx.yaml
+  sudo wget -q https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/ftw/.ftw.apache.yaml -O /etc/modsecurity/.ftw.apache.yaml
+  sudo wget -q https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/ftw/.ftw.nginx.yaml -O /etc/modsecurity/.ftw.nginx.yaml
 
   # Albedo systemd config
-  sudo wget https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/albedo/albedo.service -O /etc/systemd/system/albedo.service
+  sudo wget -q https://raw.githubusercontent.com/EsadCetiner/crs-dev-environment-setup/refs/heads/main/config/albedo/albedo.service -O /etc/systemd/system/albedo.service
   sudo systemctl daemon-reload
   sudo systemctl disable albedo --now
 
